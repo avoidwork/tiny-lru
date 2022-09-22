23175cbf2ad6ac73d015018a9a2d7985d0e6344f
Aarne Laur <aarne.laur@gmail.com>
2022-09-22 18:09:30 +0200

    fix: Manual typings with generics

--- lru.d.ts
+++ lru.d.ts
@@ -1,18 +1,18 @@
-export function lru(max?: number, ttl?: number): LRU;
-declare class LRU {
-    constructor(max?: number, ttl?: number);
-    first: any;
-    items: any;
-    last: any;
+export function lru<T = any>(max?: number, ttl?: number): LRU<T>;
+export interface LRU<T> {
+    first: T;
+    items: Record<string, T>;
+    last: T;
     max: number;
     size: number;
     ttl: number;
     has(key: any): boolean;
-    clear(): LRU;
-    delete(key: any): LRU;
-    evict(bypass?: boolean): LRU;
-    get(key: any): any;
+    clear(): LRU<T>;
+    delete(key: any): LRU<T>;
+    evict(bypass?: boolean): LRU<T>;
+    get(key: any): T;
     keys(): string[];
-    set(key: any, value: any, bypass?: boolean): LRU;
+    set(key: any, value: T, bypass?: boolean): LRU<T>;
 }
-export {};
+export { };
+
