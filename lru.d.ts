517ef2d6f09d26553c98c42d7e2b25a537f514bb
Aarne Laur <aarne.laur@gmail.com>
2022-09-22 18:13:21 +0200

    fix: More accurate types

--- lru.d.ts
+++ lru.d.ts
@@ -1,18 +1,18 @@
 export function lru<T = any>(max?: number, ttl?: number): LRU<T>;
 export interface LRU<T> {
-    first: T;
-    items: Record<string, T>;
-    last: T;
+    first: T | null;
+    last: T | null;
     max: number;
     size: number;
     ttl: number;
+
     has(key: any): boolean;
-    clear(): LRU<T>;
-    delete(key: any): LRU<T>;
-    evict(bypass?: boolean): LRU<T>;
-    get(key: any): T;
+    clear(): this;
+    delete(key: any): this;
+    evict(bypass?: boolean): this;
+    get(key: any): T | undefined;
     keys(): string[];
-    set(key: any, value: T, bypass?: boolean): LRU<T>;
+    set(key: any, value: T, bypass?: boolean): this;
 }
 export { };
 
