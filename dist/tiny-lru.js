f1820e2967545050c8cd5aa5bfd969e7d6218ed1
Jason Mulligan <jason.mulligan@avoidwork.com>
2024-07-05 23:23:07 -0400

    Updating 'files' in package.json

--- dist/tiny-lru.js
+++ dist/tiny-lru.js
@@ -3,7 +3,7 @@
  *
  * @copyright 2024 Jason Mulligan <jason.mulligan@avoidwork.com>
  * @license BSD-3-Clause
- * @version 11.2.10
+ * @version 11.2.11
  */
 class LRU {
 	constructor (max = 0, ttl = 0, resetTtl = false) {
