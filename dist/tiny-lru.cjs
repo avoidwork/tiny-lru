4f093dbe2fd56c463db35d0863c56fb5fbc2daa3
Jason Mulligan <jason.mulligan@avoidwork.com>
2024-07-05 22:47:51 -0400

    Updating types file (#216)
    * Generating types file
    
    * Updating workflow jobs
    
    * Updating README.md
    
    * Building to release
    
    * Setting generics in type file
    
    * Minor fixes
    
    * Minor fixes
    
    * Minor fixes

--- dist/tiny-lru.cjs
+++ dist/tiny-lru.cjs
@@ -3,7 +3,7 @@
  *
  * @copyright 2024 Jason Mulligan <jason.mulligan@avoidwork.com>
  * @license BSD-3-Clause
- * @version 11.2.6
+ * @version 11.2.7
  */
 'use strict';
 
--- dist/tiny-lru.umd.js
+++ dist/tiny-lru.umd.js
@@ -3,7 +3,7 @@
  *
  * @copyright 2024 Jason Mulligan <jason.mulligan@avoidwork.com>
  * @license BSD-3-Clause
- * @version 11.2.6
+ * @version 11.2.7
  */
 (function(g,f){typeof exports==='object'&&typeof module!=='undefined'?f(exports):typeof define==='function'&&define.amd?define(['exports'],f):(g=typeof globalThis!=='undefined'?globalThis:g||self,f(g.lru={}));})(this,(function(exports){'use strict';class LRU {
 	constructor (max = 0, ttl = 0, resetTtl = false) {
