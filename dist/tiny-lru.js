07afc898b21158462044400c38de9582fa845bee
Jason Mulligan <jason.mulligan@avoidwork.com>
2026-09-15 06:59:25 -0400

    chore: release v13.0.1

--- dist/tiny-lru.js
+++ dist/tiny-lru.js
@@ -3,7 +3,7 @@
  *
  * @copyright 2026 Jason Mulligan <jason.mulligan@avoidwork.com>
  * @license BSD-3-Clause
- * @version 13.0.0
+ * @version 13.0.1
  */
 /**
  * A high-performance Least Recently Used (LRU) cache implementation with optional TTL support.
